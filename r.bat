194992cd412fef8f8634cbee51b775187e61d660
fr4nd3x <francisco-dg-1@hotmail.com>
2022-10-03 12:09:16 -0500

    service

--- r.bat
+++ r.bat
@@ -1,3 +1,4 @@
 :: Int to pipenv and run a local host (http://localhost:5000/)
 :: You must to put the letter "r".
-pipenv run flask --app app run
+::pipenv run flask --app app run
+pipenv run waitress-serve wsqi:app
